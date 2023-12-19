ed66b17b5124166b138f2e3a2e204b5894ff4a6f
brianvikonura <vikonura@gmail.com>
2023-12-19 22:13:57 +0700

    Continue API integration

--- pages/companies/_id/roles/index.vue
+++ pages/companies/_id/roles/index.vue
@@ -42,64 +42,15 @@
             </div>
 
             <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-[30px]">
-                <div class="items-center card !flex-row gap-4">
+                    <p v-if="$fetchState.pending">Fetching roles...</p>
+                    <div class="items-center card !flex-row gap-4" v-else v-for="roles in roles.data.result.data" :key="roles.id">
                     <a href="#" class="absolute inset-0 focus:ring-2 ring-primary rounded-[26px]"></a>
-                    <img src="/assets/svgs/ric-flag.svg" alt="">
+                    <img src="/assets/svgs/ric-flag.svg" alt="" />
                     <div>
                         <div class="mb-1 font-semibold text-dark">
-                            Product Designer
+                            {{ roles.name }}
                         </div>
-                        <p class="text-grey">
-                            12 people assigned
-                        </p>
-                    </div>
-                </div>
-                <div class="items-center card !flex-row gap-4">
-                    <a href="#" class="absolute inset-0 focus:ring-2 ring-primary rounded-[26px]"></a>
-                    <img src="/assets/svgs/ric-flag.svg" alt="">
-                    <div>
-                        <div class="mb-1 font-semibold text-dark">
-                            iOS Engineer
-                        </div>
-                        <p class="text-grey">
-                            12 people assigned
-                        </p>
-                    </div>
-                </div>
-                <div class="items-center card !flex-row gap-4">
-                    <a href="#" class="absolute inset-0 focus:ring-2 ring-primary rounded-[26px]"></a>
-                    <img src="/assets/svgs/ric-flag.svg" alt="">
-                    <div>
-                        <div class="mb-1 font-semibold text-dark">
-                            Marketing
-                        </div>
-                        <p class="text-grey">
-                            12 people assigned
-                        </p>
-                    </div>
-                </div>
-                <div class="items-center card !flex-row gap-4">
-                    <a href="#" class="absolute inset-0 focus:ring-2 ring-primary rounded-[26px]"></a>
-                    <img src="/assets/svgs/ric-flag.svg" alt="">
-                    <div>
-                        <div class="mb-1 font-semibold text-dark">
-                            DevOps Power
-                        </div>
-                        <p class="text-grey">
-                            12 people assigned
-                        </p>
-                    </div>
-                </div>
-                <div class="items-center card !flex-row gap-4">
-                    <a href="#" class="absolute inset-0 focus:ring-2 ring-primary rounded-[26px]"></a>
-                    <img src="/assets/svgs/ric-flag.svg" alt="">
-                    <div>
-                        <div class="mb-1 font-semibold text-dark">
-                            Quality Assurance
-                        </div>
-                        <p class="text-grey">
-                            12 people assigned
-                        </p>
+                        <p class="text-grey">{{ roles.responsibilities_count }} responsibilities</p>
                     </div>
                 </div>
             </div>
@@ -111,5 +62,18 @@
 export default {
   layout: 'dashboard',
   middleware: 'auth',
+  data() {
+      return {
+          roles: [],
+      }
+    }, 
+    async fetch() {
+      this.roles = await this.$axios.get('/role', {
+        params: {
+            company_id: this.$route.params.id,
+            limit: 100,
+        }
+      })
+    },
 }
 </script>
